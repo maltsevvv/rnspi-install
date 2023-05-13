d02cbb6e41bdbf82cfe8307bd79d0377fc478215
Viachaslau <maltsev.v.v@hotmail.com>
2023-05-13 19:23:51 +0300

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,52 +1,62 @@
 #!/bin/bash
 
-# sudo sh update.sh
-BWhite='\033[1;37m'; RED='\033[0;31m'; GREEN='\033[0;32m'; NC='\033[0m' # color
+BWhite='\033[1;37m'; BBlue='\033[1;34m'; RED='\033[0;31m'; GREEN='\033[0;32m'; NC='\033[0m' # color
 if
 [ $(id -u) -ne 0 ]; then echo "Please run as root"; exit 1; fi
 
-
-echo ${BWhite}"Check file on SD card in /home/pi/ skin.rnsd.zip or skin.rnse.zip"${NC}
-if [ -e /home/pi/skin.rnsd-main.zip ]; then echo ${GREEN}"OK"${NC}; elif [ -e /home/pi/skin.rnse-main.zip ]; then echo ${GREEN}"OK"${NC}; else echo ${RED}"SKIN not found"${NC}; exit 0; fi
+if grep -Fxq 'VERSION="11 (bullseye)"' '/etc/os-release'; then echo ${GREEN}"You using Raspbian Bullseye"${NC}
+elif grep -Fxq 'VERSION="10 (buster)"' '/etc/os-release'; then echo ${GREEN}"You using Raspbian Buster"${NC}
+fi 
 echo
-	if (systemctl -q is-active kodi.service); then
-		echo ${BWhite}"stop kodi (10sec.)"${NC}
-		systemctl stop kodi.service
-		sleep 10
-	elif (systemctl -q is-active kodi.service); then
-		systemctl stop kodi.service
-		sleep 10
-	fi
 
-	# chek tvtuner.service
-	if (systemctl -q is-active tvtuner.service); then
-		echo ${BWhite}"stop tvtuner (5sec.)"${NC}
-		systemctl stop tvtuner.service
-		sleep 5
-	elif (systemctl -q is-active tvtuner.service); then
-		systemctl stop tvtuner.service
-		sleep 5
-	fi
+echo ${BWhite}"START UPDATE"${NC}
+##############################################
+#                 STOP KODI                  #
+##############################################
+if (systemctl -q is-active kodi.service); then
+	echo ${BWhite}"stop kodi (10sec.)"${NC}
+	systemctl stop kodi
+	sleep 10
+elif (systemctl -q is-active kodi.service); then
+	echo ${BWhite}"stop kodi (+10sec.)"${NC}
+	systemctl stop kodi
+	sleep 10
+exit 1
+fi
+echo
 
-	# проверить файл
-	echo ${BWhite}"Check file on SD card in /home/pi/ skin.rnsd.zip or skin.rnse.zip"${NC}
-	if [ -e /home/pi/skin.rnsd.zip ]; then
-		echo ${GREEN}"found skin.rnsd"${NC}
-		unzip ~/skin.rnsd-main.zip -d /tmp/
-		mv /tmp/skin.rnsd-main /tmp/skin.rnsd
-		rm -r /home/pi/.kodi/addons/skin.rnsd
-		cp /tmp/skin.rnsd -r /home/pi/kodi/addons/
-		
-		
-	elif [ -e /home/pi/skin.rnse.zip ]; then
-		echo ${GREEN}"found skin.rnse"${NC}
-		unzip ~/skin.rnse-main.zip -d /tmp/
-		mv /tmp/skin.rnse-main /tmp/skin.rnse
-		rm -r /home/pi/.kodi/addons/skin.rnse
-		cp /tmp/skin.rnse -r /home/pi/kodi/addons/
-	fi	
-	
-	if (systemctl -q is-enabled tvtuner.service); then
-		systemctl start tvtuner.service
+
+##############################################
+#             UPDATE SKIN RNSD              #
+##############################################
+if [ -e /home/pi/skin.rns*.zip ] ; then
+	if [ -e /home/pi/.kodi/addons/skin.rnsd ] ; then
+		rm -r /home/pi/.kodi/addons/skin.rnsd/
+		# OS BULLSEYE #
+		if grep -Fxq 'VERSION="11 (bullseye)"' '/etc/os-release'; then
+			unzip /home/pi/skin.rnsd*bullseye.zip -d /home/pi/.kodi/addons/ > /dev/null 2>&1
+		# OS BUSTER #
+		elif grep -Fxq 'VERSION="10 (buster)"' '/etc/os-release'; then
+			unzip /home/pi/skin.rnsd*buster.zip -d /home/pi/.kodi/addons/ > /dev/null 2>&1
+		fi
 	fi
-	systemctl start kodi.service
+	echo ${GREEN}"SKIN.RNSD UPDATE"${NC}
+
+##############################################
+#             UPDATE SKIN RNSE              #
+##############################################
+	elif [ -e /home/pi/.kodi/addons/skin.rnse ] ; then
+		rm -r /home/pi/.kodi/addons/skin.rnse/
+		# OS BULLSEYE #
+		if grep -Fxq 'VERSION="11 (bullseye)"' '/etc/os-release'; then
+			unzip /home/pi/skin.rnse*bullseye.zip -d /home/pi/.kodi/addons/ > /dev/null 2>&1
+		# OS BUSTER #
+		elif grep -Fxq 'VERSION="10 (buster)"' '/etc/os-release'; then
+			unzip /boot/skin.rnse*buster.zip -d /home/pi/.kodi/addons/ > /dev/null 2>&1
+		fi
+		echo ${GREEN}"SKIN.RNSE UPDATE"${NC}
+	fi
+	echo ${GREEN}"UPDATE SKIN FINISH"${NC}
+fi
+#
+systemctl start kodi
